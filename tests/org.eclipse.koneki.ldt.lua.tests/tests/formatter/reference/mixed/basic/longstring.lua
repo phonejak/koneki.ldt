d3128237094629a1e3d3327dcf2ddcb1e930616a
Kevin KIN-FOO <kkinfoo@sierrawireless.com>
2013-10-01 17:14:37 +0200

    Mixed indentation tests.

--- tests/org.eclipse.koneki.ldt.lua.tests/tests/formatter/reference/mixed/basic/longstring.lua
+++ tests/org.eclipse.koneki.ldt.lua.tests/tests/formatter/reference/mixed/basic/longstring.lua
@@ -1,7 +1,7 @@
 ---
 -- Long string to be process
 --
-local longString = [=[Long string
+local string = [[Long string
     4 space at the start of this line
 4 space at the end of this line    
-]=]
+]]
--- tests/org.eclipse.koneki.ldt.lua.tests/tests/formatter/reference/mixed/basic/longstring2.lua
+++ tests/org.eclipse.koneki.ldt.lua.tests/tests/formatter/reference/mixed/basic/longstring2.lua
@@ -1,7 +1,7 @@
 ---
 -- Long string to be process
 --
-local string = [[Long string
+local longString = [=[Long string
     4 space at the start of this line
 4 space at the end of this line    
-]]
+]=]
